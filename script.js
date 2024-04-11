23552c6ca963e3fde9354e1ddf240e57cc18174f
Vinicius E Santo <viniciusesant@gmail.com>
2024-04-10 21:50:31 -0300

    Att Projeto
    Atualização geral do projeto
    - Adição de novas imagens
    - Atualização do menu slick
    - Acréscimos de linhas com novas imagens

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
     $('#carousel-imgs').slick({
-        autoplay:true,
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 100,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    })
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle();
     })
 })
