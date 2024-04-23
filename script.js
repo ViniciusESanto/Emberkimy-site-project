0cb241c3695a8d55046376842f088fc9ab118649
Vinicius E Santo <viniciusesant@gmail.com>
2024-04-23 11:28:59 -0300

    Att Projeto
    - atualização e adição de plugins JQuery

--- script.js
+++ script.js
@@ -11,4 +11,42 @@ $(document).ready(function(){
     $('.menu-hamburguer').click(function(){
         $('nav').slideToggle();
     })
+
+    $('#telefone').mask('(00) 00000-0000')
+    
+    $('form').validate({
+        rules: {
+            nome: {
+                required:true
+            },
+            email: {
+                required:true,
+                email:true
+            },
+            telefone: {
+                required:true
+            },
+            interesse: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Porfavor, insira seu nome!',
+            telefone: 'Este campo é obrigatorio!',
+            email: 'Este campo é obrigatorio!'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            console.log(camposIncorretos)
+            if(camposIncorretos) {
+                alert(`Exitem ${camposIncorretos} a serem preenchidos`)
+            }
+        }
+    })
 })
